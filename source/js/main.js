fcc334dc913bfc1a0a180307901b0f25156d63f2
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-08-07 12:20:34 +0300

    Доработки

--- source/js/main.js
+++ source/js/main.js
@@ -2,11 +2,4 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
-
-const burgerButton = document.querySelector('.page-header__burger');
-const navigation = document.querySelector('.page-header__navigation');
-
-burgerButton.addEventListener('click', () => {
-  burgerButton.classList.toggle('page-header__burger--cross');
-  navigation.classList.toggle('page-header__navigation--open');
-});
+import './menu';
---
+++ source/js/menu.js
@@ -0,0 +1,20 @@
+const burgerButton = document.querySelector('.page-header__burger');
+const navigation = document.querySelector('.page-header__navigation');
+
+const toggleMenu = () => {
+  burgerButton.classList.toggle('page-header__burger--cross');
+  navigation.classList.toggle('page-header__navigation--open');
+  document.body.classList.toggle('body--scroll-lock');
+};
+
+burgerButton.addEventListener('click', () => {
+  toggleMenu();
+});
+
+navigation.addEventListener('click', (e) => {
+  const element = e.target;
+  if(!element.closest('.page-header__item-link')){
+    return;
+  }
+  toggleMenu();
+});
