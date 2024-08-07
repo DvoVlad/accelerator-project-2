38407f4d6f8d86f6fb5dae1d744cc579409e6256
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-08-07 10:26:25 +0300

    Сделал меню

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,11 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+const burgerButton = document.querySelector('.page-header__burger');
+const navigation = document.querySelector('.page-header__navigation');
+
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('page-header__burger--cross');
+  navigation.classList.toggle('page-header__navigation--open');
+});
