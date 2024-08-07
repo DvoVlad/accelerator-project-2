1c562647c807f5e7b9e34218a1b23877e745dbc0
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-08-07 14:26:36 +0300

    Сделал слайдер на мобильной

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,17 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
+import {Navigation, Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import './menu';
+
+new Swiper('.hero__swiper', {
+  modules: [Navigation, Pagination],
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.hero__swiper-pagination',
+  }
+});
